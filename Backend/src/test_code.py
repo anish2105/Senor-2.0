5737bd06fe5a97ce38869a3b6a0f5453f8d8e729
anish2105 <anishvv212002@gmail.com>
2025-03-09 19:01:27 +0530

    first commit

---
+++ Backend/src/test_code.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+load_dotenv()
+from langchain_google_genai import ChatGoogleGenerativeAI
+import os
+
+
+## call the gemini models
+llm=ChatGoogleGenerativeAI(model="gemini-1.5-flash",
+                           verbose=True,
+                           temperature=0.1,
+                           google_api_key=os.getenv("GOOGLE_API_KEY"))
